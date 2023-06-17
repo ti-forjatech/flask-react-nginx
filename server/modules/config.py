ce506825d98c5deb2ed2b92e189dcaddff2ca66d
Thyez de Oliveira <thyezoliveira@gmail.com>
2023-06-16 21:34:27 -0300

    Atualizar o config.py
    Passei a expiração do token para 24hra

--- server/modules/config.py
+++ server/modules/config.py
@@ -18,7 +18,7 @@ def define_app_secret(app):
         appconfig.write(new_appconfig_data)
     
     app.secret_key = config['SECRET_KEY']
-    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=24)
     app.config["JWT_COOKIE_SECURE"] = False
     app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
     app.config["JWT_SECRET_KEY"] = config['SECRET_KEY']
