d157664372ba4e25543d4ec3b2b048d5eeaacc31
Thyéz <thyezoliveira@gmail.com>
2023-06-19 08:41:28 -0300

    d

--- server/modules/connection.py
+++ server/modules/connection.py
@@ -5,7 +5,7 @@ url_object = URL.create(
     "mysql",
     username=os.environ.get("MYSQL_USER"),
     password=os.environ.get("MYSQL_PASSWORD"),
-    host="rioservice_db",
+    host="127.0.0.1",
     database=os.environ.get("MYSQL_DATABASE"),
     port=3306
 )
